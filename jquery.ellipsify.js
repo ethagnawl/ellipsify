7173a5ec1a90ebe6b1462cd5437396f7ddf88d13
pete <pete@kafka.(none)>
2010-08-19 16:59:43 -0400

    cleaning up inner loops

--- jquery.ellipsify.js
+++ jquery.ellipsify.js
@@ -14,36 +14,37 @@
 		var config = {
 					count: 25,
 					ellip: '...',
-					split_join: ' ',
 					no_ellipsify: 'Not enough text to ellipsify, try passing in a lower count arg if ellipsification is desired.',
+					split_join: ' ',
 					type: 'words'
 				}
+			,	count = 0
+			,	inner
+			,	inner_count_arr = []
+			,	prev
 			,	console = function(msg){
 					if(window.console){
 						window.console.log(msg);
 					}	
-			}	
-			,	count = 0
-			,	inner
-			,	inner_count_arr = []
+				}	
 			,	maker = function(inner, count, elem){
 
 					var	$elem = $(elem)
 						,	last_word = inner[(count-1)]
 					;
 
-					// check to see if last word ends with "." and if so, remove it
-					// "last_word..." is prettier than "last_word...."
+					//	check to see if last word ends with "." and if so, remove it
+					//	"last_word..." is prettier than "last_word...."
 					if((/\.$/.test(last_word))){
 
 						inner[(count-1)] = last_word.replace('.', '');
 
 					}
 
-					// add "string..." or "string <a href='/'>Link</a>" back into elem
+					//	add "string..." or "string <a href='/'>Link</a>" back into elem
 					$elem.addClass('ellipsis').html((inner.splice(0, count)).join(config.split_join)+config.ellip);
 
-					// if elem has subsequent siblings, remove them
+					//	if elem has subsequent siblings, remove them
 					if($elem.next().length>0){
 						$elem.nextAll().remove();
 					}	
@@ -51,17 +52,17 @@
 				}		
 		;
 		
-		// override config if user supplied options
+		//	override config if user supplied options
 		if(options){
 		
 			$.extend(config, options);
 			
-			// set split/join on chars or words	
+			//	set split/join on chars or words	
 			config.split_join = (config.type === 'words') ? ' ' : '';
 			
 		}
 
-		// only 1 elem
+		//	1 elem
 		if(this.length === 1){
 
 			inner = $(this).text().split(config.split_join);
@@ -80,12 +81,11 @@
 
 		}
 
-		// more than 1 elem
+		//	more than 1 elem
 		else if(this.length > 1){
 
 			$.each(this, function(i){
 
-				// loop vars	
 				inner = $(this).text().split(config.split_join)
 					,	inner_count = inner.length
 					,	total = inner_count + count
@@ -95,27 +95,27 @@
 				// add elem's inner length to inner_count_arr
 				inner_count_arr.push(inner_count);
 
-				// if the end point is in this elem...
+				//	if the end point is in this elem...
 				if(total > config.count){
 
-					// loop through each split in the array until break point
-					for (var z = 0; z < inner.length; z++){
+					//	loop through indices until count matches config.count
+					while(count<config.count){
 
 						count++;
 
-						// if this split point is the end... 
+						//	if this index matches config.count... 
 						if (count === config.count){
 
-							// if this elem is the first in the collection
-							// operate normally 
-							if (prev === -1){
+							//	if this elem is the first in the collection
+							//	operate normally 
+							if (prev < 0){
 
 								maker(inner, count, this);
 
 							}
 
-							// else operate on the elem containing the break point
-							// and not prior elems
+							//	else operate on the elem containing the break point
+							//	and not prior elems
 							else{
 
 								maker(inner, count - inner_count_arr[prev], this);
