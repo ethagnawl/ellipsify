dfad07230ed1df2e5bfeb24adebb2a11e74b5d57
pete <pete@kafka.(none)>
2010-08-31 00:30:08 -0400

    adding GPL license

--- jquery.ellipsify.js
+++ jquery.ellipsify.js
@@ -5,6 +5,20 @@
 *	http://ethagnawl.com/ellipsify
 *	Copyright 2010, Pete Doherty
 *	Date: 08/30/2010 23:22:16 (EST)
+*
+*   ellipsify is distributed under the terms of the GNU General Public License.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   GNU General Public License for more details.
+*
+*   http://www.gnu.org/licenses/gpl.txt
 */
 
 (function($){
