89961288d1bcfca5d8b474c3a7d8543b7f8abe38
pete <pete@kafka.(none)>
2010-08-31 00:26:17 -0400

    checking in ellipsify v1.2 - major refactoring, comments to come...

--- jquery.ellipsify.js
+++ jquery.ellipsify.js
@@ -1,157 +1,61 @@
 /*!
 *	ellipsify jQuery Plugin
-*	V1.1
+*	V1.2
 *	ethagnawl@gmail.com
 *	http://ethagnawl.com/ellipsify
 *	Copyright 2010, Pete Doherty
-*	Date: Thu Aug 19 2010 02:14:04
+*	Date: 08/30/2010 23:22:16 (EST)
 */
 
 (function($){
-
 	$.fn.ellipsify = function(options){
-
-		var config = {
+		var no_space = ''
+            ,   space = ' '
+            ,   words = 'words'
+            ,   config = {
 					count: 25,
 					ellip: '...',
-					no_ellipsify: 'Not enough text to ellipsify, try passing in a lower count arg if ellipsification is desired.',
-					split_join: ' ',
-					type: 'words'
+					ellipsify_class: 'ellipsis',
+					split_join: space,
+					type: words
 				}
-			,	count = 0
-			,	inner
-			,	inner_count_arr = []
-			,	prev
-			,	console = function(msg){
-					if(window.console){
-						window.console.log(msg);
-					}	
-				}	
-			,	maker = function(inner, count, elem){
-
-					var	$elem = $(elem)
-						,	last_word = inner[(count-1)]
-					;
-
-					//	check to see if last word ends with "." and if so, remove it
-					//	"last_word..." is prettier than "last_word...."
-					if((/\.$/.test(last_word))){
-
-						inner[(count-1)] = last_word.replace('.', '');
-
-					}
-
-					//	add "string..." or "string <a href='/'>Link</a>" back into elem
-					$elem.addClass('ellipsis').html((inner.splice(0, count)).join(config.split_join)+config.ellip);
-
-					//	if elem has subsequent siblings, remove them
-					if($elem.next().length>0){
-						$elem.nextAll().remove();
-					}	
-
+			,   length = this.length
+			,   no_ellipsify = 'Not enough text to ellipsify, try passing in a lower count arg if ellipsification is desired.'
+			,	console = function (msg) {
+		            if (window.console) window.console.log(msg);
+	            }	
+			,	maker = function (inner, count, $elem) {
+			        var last_word = inner[count-1];
+					if (/\.$/.test(last_word)) inner[count-1] = last_word.replace('.', no_space);
+					$elem.addClass(config.ellipsify_class).html(inner.slice(0, count).join(config.split_join) + config.ellip);
+					if ($elem.next().length) $elem.nextAll().remove();
 				}		
 		;
-		
-		//	override config if user supplied options
-		if(options){
-		
+		if (options) {
 			$.extend(config, options);
-			
-			//	set split/join on chars or words	
-			config.split_join = (config.type === 'words') ? ' ' : '';
-			
+			config.split_join = config.type === words ? space : no_space;
 		}
-
-		//	1 elem
-		if(this.length === 1){
-
-			inner = $(this).text().split(config.split_join);
-			
-			if(inner.length >= config.count){
-
-				maker(inner, config.count, this);
-
-			}
-
-			else{
-
-				console(config.no_ellipsify);
-
-			}	
-
-		}
-
-		//	more than 1 elem
-		else if(this.length > 1){
-
-			$.each(this, function(i){
-
-				inner = $(this).text().split(config.split_join)
-					,	inner_count = inner.length
-					,	total = inner_count + count
-					,	prev = i - 1
-				;
-
-				// add elem's inner length to inner_count_arr
-				inner_count_arr.push(inner_count);
-
-				//	if the end point is in this elem...
-				if(total > config.count){
-
-					//	loop through indices until count matches config.count
-					while(count<config.count){
-
-						count++;
-
-						//	if this index matches config.count... 
-						if (count === config.count){
-
-							//	if this elem is the first in the collection
-							//	operate normally 
-							if (prev < 0){
-
-								maker(inner, count, this);
-
-							}
-
-							//	else operate on the elem containing the break point
-							//	and not prior elems
-							else{
-
-								maker(inner, count - inner_count_arr[prev], this);
-
-							}
-
-						}
-
-					}
-	
-				}
-
-				else{
-
-					count += inner_count;
-
-				}
-				
-			});
-
-			if(count < config.count){
-
-				console(config.no_ellipsify);
-
-			}
-		
-		}
-
-		else{
-		
+		if (length < 1) {
 			console('No elements to ellipsify.');	
-			
+		} else if (length === 1) {
+			var inner = this.get(0).innerHTML.split(config.split_join);
+			inner.length < config.count ? console(no_ellipsify) : maker(inner, config.count, this);
+		} else {
+			var count = 0, inner_count_arr = [], inner_length, prev, $that;
+			$.each(this, function (i) {
+                var $that = $(this), inner = $that.get(0).innerHTML.split(config.split_join);
+                inner_length = inner.length, prev = i - 1;
+				inner_count_arr.push(inner_length);
+				if (inner_length + count < config.count) {
+                    count += inner_length;
+                } else {
+					while (count < config.count) {
+						count++;
+					}
+                    maker(inner, prev < 0 ? config.count : count - inner_count_arr[prev], $that);
+				}
+			});
 		}
-
 		return this;
-
 	};
-
 })(jQuery);
